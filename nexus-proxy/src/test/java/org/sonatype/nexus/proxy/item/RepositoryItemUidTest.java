4f048cd7219bfef7a545b8e0d5be971fe6b10cc4
mpowers <mpowers@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2008-07-09 20:55:44 +0000

    NEXUS-194, NEXUS-200: changes merged from trunk
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1.0.0-beta-4.1@638 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-proxy/src/test/java/org/sonatype/nexus/proxy/item/RepositoryItemUidTest.java
+++ nexus-proxy/src/test/java/org/sonatype/nexus/proxy/item/RepositoryItemUidTest.java
@@ -74,7 +74,7 @@ public class RepositoryItemUidTest
         thread.start();
         thread2.start();
         
-        Thread.sleep( 5 );
+        Thread.sleep( 10 );
         
         assertEquals( 1, RepositoryItemUid.getLockCount() );
         
