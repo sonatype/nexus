ddaf439017a2e1c7c831f96c378b138a24cfe370
dip <dip@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-01-08 17:43:19 +0000

    NEXUS-1339: typo
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1.2.x/nexus@2955 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-app/src/main/java/org/sonatype/nexus/maven/tasks/descriptors/RebuildMavenMetadataTaskDescriptor.java
+++ nexus-app/src/main/java/org/sonatype/nexus/maven/tasks/descriptors/RebuildMavenMetadataTaskDescriptor.java
@@ -30,7 +30,7 @@ import org.sonatype.nexus.tasks.descriptors.AbstractScheduledTaskDescriptor;
 import org.sonatype.nexus.tasks.descriptors.ScheduledTaskDescriptor;
 import org.sonatype.nexus.tasks.descriptors.properties.ScheduledTaskPropertyDescriptor;
 
-@Component( role = ScheduledTaskDescriptor.class, hint = "RebuildMavenMetadata", description = "Rebuild Maven Medata Files" )
+@Component( role = ScheduledTaskDescriptor.class, hint = "RebuildMavenMetadata", description = "Rebuild Maven Metadata Files" )
 public class RebuildMavenMetadataTaskDescriptor
     extends AbstractScheduledTaskDescriptor
 {
@@ -47,7 +47,7 @@ public class RebuildMavenMetadataTaskDescriptor
 
     public String getName()
     {
-        return "Rebuild Maven Medata Files";
+        return "Rebuild Maven Metadata Files";
     }
 
     public List<ScheduledTaskPropertyDescriptor> getPropertyDescriptors()
