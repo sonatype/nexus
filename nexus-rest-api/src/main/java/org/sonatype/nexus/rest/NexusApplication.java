24cb545c4cc492b50da925da085bd9650814df29
mpowers <mpowers@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-03-10 19:40:52 +0000

    NEXUS-1771: merged to 1.3.x branch
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1.3.X@3615 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-rest-api/src/main/java/org/sonatype/nexus/rest/NexusApplication.java
+++ nexus-rest-api/src/main/java/org/sonatype/nexus/rest/NexusApplication.java
@@ -564,7 +564,7 @@ public class NexusApplication
         attach( root, true, indexTemplateResource );
 
         // publish the WAR contents
-        Directory rootDir = new Directory( getContext(), "war:///" );
+        Directory rootDir = new NexusDirectory( getContext(), "war:///" );
         rootDir.setListingAllowed( false );
         rootDir.setNegotiateContent( false );
         attach( root, false, "/", rootDir );
@@ -582,7 +582,7 @@ public class NexusApplication
                 {
                     for ( StaticResource resource : resources )
                     {
-                        attach( root, true, resource.getPath(), new StaticResourceFinder( getContext(), resource ) );
+                        attach( root, false, resource.getPath(), new StaticResourceFinder( getContext(), resource ) );
                     }
                 }
             }
