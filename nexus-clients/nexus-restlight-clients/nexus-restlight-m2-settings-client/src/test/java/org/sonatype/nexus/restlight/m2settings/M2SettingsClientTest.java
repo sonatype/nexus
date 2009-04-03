f2b149fe660f8c191a1661d64ae0b02fb69fce5e
jdcasey <jdcasey@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-04-03 01:51:15 +0000

    Adding license headers.
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1.3.X@3812 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-clients/nexus-restlight-clients/nexus-restlight-m2-settings-client/src/test/java/org/sonatype/nexus/restlight/m2settings/M2SettingsClientTest.java
+++ nexus-clients/nexus-restlight-clients/nexus-restlight-m2-settings-client/src/test/java/org/sonatype/nexus/restlight/m2settings/M2SettingsClientTest.java
@@ -1,3 +1,23 @@
+/*
+ * Nexus: RESTLight Client
+ * Copyright (C) 2009 Sonatype, Inc.                                                                                                                          
+ * 
+ * This file is part of Nexus.                                                                                                                                  
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
 package org.sonatype.nexus.restlight.m2settings;
 
 import static org.junit.Assert.assertEquals;
@@ -8,7 +28,6 @@ import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 import org.junit.Test;
 import org.sonatype.nexus.restlight.common.RESTLightClientException;
-import org.sonatype.nexus.restlight.m2settings.M2SettingsClient;
 import org.sonatype.nexus.restlight.testharness.AbstractRESTTest;
 import org.sonatype.nexus.restlight.testharness.ConversationalFixture;
 import org.sonatype.nexus.restlight.testharness.GETFixture;
@@ -24,7 +43,7 @@ public class M2SettingsClientTest
     extends AbstractRESTTest
 {
     
-    private ConversationalFixture fixture = new ConversationalFixture();
+    private final ConversationalFixture fixture = new ConversationalFixture();
     
     @Test
     public void getSettingsTemplateUsingToken()
--- nexus-clients/nexus-restlight-clients/nexus-restlight-stage-client/src/test/java/org/sonatype/nexus/restlight/stage/StageClientTest.java
+++ nexus-clients/nexus-restlight-clients/nexus-restlight-stage-client/src/test/java/org/sonatype/nexus/restlight/stage/StageClientTest.java
@@ -1,3 +1,23 @@
+/*
+ * Nexus: RESTLight Client
+ * Copyright (C) 2009 Sonatype, Inc.                                                                                                                          
+ * 
+ * This file is part of Nexus.                                                                                                                                  
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ */
 package org.sonatype.nexus.restlight.stage;
 
 import static org.junit.Assert.assertEquals;
@@ -7,8 +27,6 @@ import static org.junit.Assert.fail;
 import org.jdom.JDOMException;
 import org.junit.Test;
 import org.sonatype.nexus.restlight.common.RESTLightClientException;
-import org.sonatype.nexus.restlight.stage.StageClient;
-import org.sonatype.nexus.restlight.stage.StageRepository;
 import org.sonatype.nexus.restlight.testharness.AbstractRESTTest;
 import org.sonatype.nexus.restlight.testharness.ConversationalFixture;
 import org.sonatype.nexus.restlight.testharness.GETFixture;
@@ -24,7 +42,7 @@ public class StageClientTest
     extends AbstractRESTTest
 {
 
-    private ConversationalFixture fixture = new ConversationalFixture();
+    private final ConversationalFixture fixture = new ConversationalFixture();
 
     @Test
     public void queryAllOpenRepositoriesForUser()
