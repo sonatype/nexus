984727d57f958860ae4f1789200a814947380551
dbradicich <dbradicich@2aa8b3fc-8ebb-4439-a84f-95066eaea8ab>
2009-10-05 17:46:53 +0000

    merged dips UI fixes back to 1.4.0
    git-svn-id: file:///opt/svn/repositories/sonatype.org/nexus/branches/nexus-1.4.0@5172 2aa8b3fc-8ebb-4439-a84f-95066eaea8ab

--- nexus-webapp/src/main/webapp/js/repoServer/repoServer.UserPrivilegeBrowserPanel.js
+++ nexus-webapp/src/main/webapp/js/repoServer/repoServer.UserPrivilegeBrowserPanel.js
@@ -66,7 +66,7 @@ Sonatype.repoServer.UserPrivilegeBrowsePanel = function( config ) {
     height: '100%',
     autoScroll: true,
     border: false,
-    frame: true,
+    frame: false,
     collapsible: false,
     collapsed: false,
     tbar: [
@@ -81,17 +81,21 @@ Sonatype.repoServer.UserPrivilegeBrowsePanel = function( config ) {
     items : [
         {
           xtype: 'panel',
-          layout: 'column',
+          layout: 'table',
+          layoutConfig: {
+        	columns: 2
+          },
+          frame: true,
           items: [
             {
               xtype :'panel',
-              layout :'auto',
-              columnWidth: .5,
-              items :[
+              style :'padding: 10px; font-size: 11px;',
+              html: 'Select a privilege to view the role(s) in the user<br>that grant the privilege.'
+            },
                 {
                   xtype: 'panel',
-                  style :'padding: 10px 0 10px 0',
-                  html: 'Select a privilege to view the role(s) in the user<br>that grant the privilege.'
+                style :'padding: 10px; font-size: 11px;',
+                html: 'List of roles in the user that grant the selected privilege.<br>Expand the role to find nested role(s) that contain<br>the privilege.'
                 },
                 {
                   xtype :'treepanel',                  
@@ -100,7 +104,7 @@ Sonatype.repoServer.UserPrivilegeBrowsePanel = function( config ) {
                   border :true,
                   bodyBorder :true,
                   bodyStyle :'background-color:#FFFFFF; border: 1px solid #B5B8C8',
-                  style :'padding: 0 50px 0 0',
+              style :'padding: 0 20px 0 0',
                   width :325,
                   height :275,
                   animate :true,
@@ -114,20 +118,8 @@ Sonatype.repoServer.UserPrivilegeBrowsePanel = function( config ) {
                     text :'root',
                     draggable: false
                   })
-                }
-              ]
             },
             {
-              xtype :'panel',
-              layout :'auto',
-              columnWidth: .5,
-              items :[
-                {
-                  xtype: 'panel',
-                  style :'padding: 10px 0 10px 0',
-                  html: 'List of roles in the user that grant the selected privilege.<br>Expand the role to find nested role(s) that contain<br>the privilege.'
-                },
-                {
                   xtype :'treepanel',
                   name :'role-tree',
                   title :'Role Containment',
@@ -151,8 +143,6 @@ Sonatype.repoServer.UserPrivilegeBrowsePanel = function( config ) {
               ]
             }
           ]
-        }
-      ]
     });
 };
 
