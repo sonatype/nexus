fc3e650ef1172f880661760f801a177205e6508b
Jason Dillon <jason@planet57.com>
2012-11-02 19:18:29 -0700

    Add plugins to test siesta integration

---
+++ nexus/nexus-core-plugins/nexus-siesta-test-plugin/src/main/java/org/sonatype/nexus/plugins/siesta/test/TestResource.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2007-2012 Sonatype, Inc.  All rights reserved.
+ * Includes the third-party code listed at http://links.sonatype.com/products/central-secure/attributions.
+ * "Sonatype" is a trademark of Sonatype, Inc.
+ */
+
+package org.sonatype.nexus.plugins.siesta.test;
+
+import org.sonatype.sisu.siesta.common.Resource;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Date;
+
+/**
+ * Siesta testing resource.
+ *
+ * @since 2.3
+ */
+@Named
+@Singleton
+@Path("/test")
+public class TestResource
+    implements Resource
+{
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String get() {
+        return String.format("Hello %s", new Date());
+    }
+}
